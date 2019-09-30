5b95381ce3d38e2c8221c28e76aaf09e49e07f2d
snaulX <alexandergunger666@gmail.com>
2019-09-30 19:13:43 +0300

    Create PrintExpression

---
+++ System/SystemFramework/System/expressions/PrintExpression.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using wolvm;
+
+namespace wolSystem.expressions
+{
+    public class PrintExpression : VMExpression
+    {
+        public Value ParseExpression(params Value[] args)
+        {
+            foreach (Value arg in args)
+            {
+                try
+                {
+                    Console.Write(((wolString)arg.type).value);
+                }
+                catch (InvalidCastException)
+                {
+                    Console.Write(arg.ToString());
+                }
+            }
+            return Value.VoidValue;
+        }
+    }
+}
