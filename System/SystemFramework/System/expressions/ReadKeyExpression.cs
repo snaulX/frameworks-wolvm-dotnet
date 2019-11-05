bbbca31afa4ea003cf98641931d8f76545aa59c7
snaulX <alexandergunger666@gmail.com>
2019-11-05 21:27:49 +0300

    Add try-catch to ReadKeyExpression

--- System/SystemFramework/System/expressions/ReadKeyExpression.cs
+++ System/SystemFramework/System/expressions/ReadKeyExpression.cs
@@ -9,10 +9,18 @@ namespace wolSystem.expressions
     {
         public Value ParseExpression(params Value[] args)
         {
-            if (((wolBool)args[0].type).value)
-                return new Value(new wolChar(Console.ReadKey().KeyChar));
-            else
-                return new Value(new wolInt((int)Console.ReadKey().Key));
+            try
+            {
+                if (((wolBool)args[0].type).value)
+                    return new Value(new wolChar(Console.ReadKey().KeyChar));
+                else
+                    return new Value(new wolInt((int)Console.ReadKey().Key));
+            }
+            catch
+            {
+                Console.ReadKey();
+                return Value.VoidValue;
+            }
         }
     }
 }
